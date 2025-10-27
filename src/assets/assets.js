0dd36443e2b0bc98f921fc9fd955fd58b1e32ee8
abhishek <abhishekkaldate365@gmail.com>
2025-10-27 19:34:16 +0530

    suryadakshin

---
+++ src/assets/assets.js
@@ -0,0 +1,373 @@
+import basket_icon from './basket_icon.png'
+
+import slogo from './slogo.png'
+
+
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import f1 from './f1.png'
+import f2 from './f2.png'
+import f3 from './f3.png'
+import f4 from './f4.png'
+import f5 from './f5.png'
+import f6 from './f6.png'
+import f7 from './f7.png'
+import f8 from './f8.png'
+import f9 from './f9.png'
+import f10 from './f10.png'
+import f11 from './f11.png'
+import f12 from './f12.png'
+import f13 from './f13.png'
+import f14 from './f14.png'
+import f15 from './f15.png'
+import f16 from './f16.png'
+import f17 from './f17.png'
+import f18 from './f18.png'
+import f19 from './f19.png'
+import f20 from './f20.png'
+import f21 from './f21.png'
+import f22 from './f22.png'
+import f23 from './f23.png'
+import f24 from './f24.png'
+import f25 from './f25.png'
+import f26 from './f26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import scholle from './scholle.png'
+import sdosa from './sdosa.png'
+import sidli from './sidli.png'
+import slassi from './slassi.png'
+import spoha from './spoha.png'
+import ssamosa from './ssamosa.png'
+import supma from './supma.png'
+import suttappa from './suttappa.png'
+
+
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    
+    slogo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon,
+    sdosa,
+    scholle,
+    sidli,
+    slassi,
+    spoha,
+    ssamosa,
+    supma,
+    suttappa
+}
+
+export const menu_list = [
+    {
+        menu_name: "cholle",
+        menu_image: scholle
+    },
+    {
+        menu_name: "idli",
+        menu_image: sidli
+    },
+    {
+        menu_name: "lassi",
+        menu_image: slassi
+    },
+    {
+        menu_name: "poha",
+        menu_image: spoha
+    },
+    {
+        menu_name: "samosa",
+        menu_image: ssamosa
+    },
+    {
+        menu_name: "upma",
+        menu_image: supma
+    },
+    {
+        menu_name: "idli",
+        menu_image: sidli
+    },
+    {
+        menu_name: "uttappa",
+        menu_image: suttappa
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "cholle ",
+        image: f4,
+        price: 120,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "cholle"
+    },
+    {
+        _id: "2",
+        name: "cholle",
+        image: f5,
+        price: 100,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "cholle"
+    }, {
+        _id: "3",
+        name: "cholle",
+        image: f6,
+        price: 120,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "cholle"
+    }, {
+        _id: "4",
+        name: "cholle",
+        image: f4,
+        price: 120,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "cholle"
+    }, {
+        _id: "5",
+        name: " idli",
+        image: f1,
+        price: 40,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "idli"
+    }, {
+        _id: "6",
+        name: "idli",
+        image: f3,
+        price: 40,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "idli"
+    }, {
+        _id: "7",
+        name: " idli",
+        image: f2,
+        price: 40,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "idli"
+    }, {
+        _id: "8",
+        name: "idli ",
+        image: f1,
+        price: 50,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "idli"
+    }, {
+        _id: "9",
+        name: "lassi",
+        image: f9,
+        price: 30,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "lassi"
+    }, {
+        _id: "10",
+        name: "lassi",
+        image: f10,
+        price: 30,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "lassi"
+    }, {
+        _id: "11",
+        name: "lassi",
+        image: f8,
+        price: 30,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "lassi"
+    }, {
+        _id: "12",
+        name: "lassi",
+        image: f8,
+        price: 30,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "lassi"
+    },
+    {
+        _id: "13",
+        name: "poha",
+        image: f13,
+        price: 30,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "poha"
+    },
+    {
+        _id: "14",
+        name: "poha",
+        image: f12,
+        price: 30,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "poha"
+    }, {
+        _id: "15",
+        name: "poha",
+        image: f11,
+        price: 30,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "poha"
+    }, {
+        _id: "16",
+        name: "samosa",
+        image: f20,
+        price: 25,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "samosa"
+    }, {
+        _id: "17",
+        name: "samosa",
+        image: f14,
+        price: 25,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "samosa"
+    }, {
+        _id: "18",
+        name: "samosa",
+        image: f15,
+        price: 25,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "samosa"
+    }, {
+        _id: "19",
+        name: "samosa",
+        image: f15,
+        price: 25,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "samosa"
+    }, {
+        _id: "20",
+        name: " upma",
+        image: f19,
+        price: 40,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "upma"
+    }, {
+        _id: "21",
+        name: "upma  ",
+        image: f20,
+        price: 40,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "upma"
+    }, {
+        _id: "22",
+        name: "upma ",
+        image: f21,
+        price: 40,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "upma"
+    }, {
+        _id: "23",
+        name: "upma  ",
+        image: f19,
+        price: 60,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "upma"
+    }, {
+        _id: "24",
+        name: "idli ",
+        image: f4,
+        price: 60,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "idli"
+    },
+    {
+        _id: "25",
+        name: "poha ",
+        image: f12,
+        price: 60,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "idli"
+    },
+    {
+        _id: "26",
+        name: " poha",
+        image: f18,
+        price: 60,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "idli"
+    }, {
+        _id: "27",
+        name: " upma",
+        image: f20,
+        price: 60,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "idli"
+    }, {
+        _id: "28",
+        name: " upma",
+        image: f21,
+        price: 60,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "idli"
+    }, {
+        _id: "29",
+        name: "uttappa ",
+        image: f22,
+        price: 100,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "uttappa"
+    }, {
+        _id: "30",
+        name: "uttappa ",
+        image: f23,
+        price: 90,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "uttappa"
+    }, {
+        _id: "31",
+        name: "uttappa ",
+        image: f24,
+        price: 90,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "uttappa"
+    }, {
+        _id: "32",
+        name: "uttappa ",
+        image: f25,
+        price: 100,
+        description: "Discover magic in every meal, made with love and freshness.",
+        category: "uttappa"
+    }
+]
