c843635d23c7e04c193390a6f1ddbe6614befb79
abhishek <abhishekkaldate365@gmail.com>
2025-10-28 16:50:32 +0530

    surya

--- src/components/Journey/Journey.jsx
+++ src/components/Journey/Journey.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Journey.css'
-import suryaaa from '../../assets/suryaaa.mp4'
+import surya from '../../assets/surya.mp4'
 
 
 
@@ -9,7 +9,7 @@ const Journey = () => {
     <div className='journey'>
       
         <h1>Success Of Our Restaurant</h1>
-        <video src={suryaaa} autoPlay muted controls></video>
+        <video src={surya} autoPlay muted controls></video>
         <p>The success of a restaurant depends on a combination of factors, including delivering high-quality and consistent food,<br/>
          providing excellent customer service, and maintaining strong financial management. Key elements also include a strong brand and concept,<br/>
          strategic location, effective marketing, and the ability to adapt to new trends and technologies like online ordering</p>
